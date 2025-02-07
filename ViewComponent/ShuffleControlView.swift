87a2eb2d7e18293ede0d311a6e4e713ff0b7dc04
JEY HIRANO <mrjr0722@me.com>
2025-02-07 10:55:51 +0900

    LockButtonViewを追加

--- ViewComponent/ShuffleControlView.swift
+++ ViewComponent/ShuffleControlView.swift
@@ -10,20 +10,9 @@ import SwiftUI
 
 struct ShuffleControlView: View {
     
-    @ViewBuilder
-    private func ShuffleButton() -> some View {
-        Button {
-            self.doShuffle()
-        } label: {
-            Image(systemName: "shuffle")
-                .padding([.leading, .trailing], 10)
-        }
-        .padding(16)
-        .foregroundColor(Color.white)
-        .background(Color.blue)
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-        .font(.title3)
-    }
+    @State private var isTopsLock: Bool = false
+    @State private var isBottomsLock: Bool = false
+    @State private var isShoesLock: Bool = false
     
     var body: some View {
         VStack {
@@ -31,6 +20,7 @@ struct ShuffleControlView: View {
                 VStack {
                     Text("ShuffleControlView")
                     Spacer()
+                    LockButtonView()
                     ShuffleButton()
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -44,6 +34,51 @@ struct ShuffleControlView: View {
         .padding(.top, 10)
         .padding([.leading, .trailing], 10)
         .padding(.bottom, 5)
+    }
+    
+    @ViewBuilder
+    private func LockButtonView() -> some View {
+        VStack {
+            Toggle(isOn: $isTopsLock, label: {
+                HStack {
+                    Text("Tops")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Image(systemName: self.isTopsLock ? "lock" : "lock.open")
+                }
+            })
+            Toggle(isOn: $isBottomsLock, label: {
+                HStack {
+                    Text("Bottoms")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Image(systemName: self.isBottomsLock ? "lock" : "lock.open")
+                }
+            })
+            Toggle(isOn: $isShoesLock, label: {
+                HStack {
+                    Text("Shoes")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Image(systemName: self.isShoesLock ? "lock" : "lock.open")
+                }
+            })
+        }
+    }
+    
+    @ViewBuilder
+    private func ShuffleButton() -> some View {
+        Button {
+            self.doShuffle()
+        } label: {
+            Image(systemName: "shuffle")
+                .padding([.leading, .trailing], 10)
+        }
+        .padding(16)
+        .foregroundColor(Color.white)
+        .background(Color.blue)
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .font(.title3)
     }
     
     private func doShuffle() {
