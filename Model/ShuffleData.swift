e029e4adc93ad0d551fd3b66adebb0be737bf899
JEY HIRANO <mrjr0722@me.com>
2025-02-14 10:30:59 +0900

    ShuffleDataクラス追加

---
+++ Model/ShuffleData.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  StyleRaku
+//  
+//  Created by Jey Hirano on 2025/02/14
+//  
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
