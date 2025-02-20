224760954015c1786c76d517b68cfae2560af2a3
JEY HIRANO <mrjr0722@me.com>
2025-02-20 11:54:25 +0900

    初期データのセット

--- MyApp.swift
+++ MyApp.swift
@@ -45,9 +45,43 @@ struct MyApp: App {
         
         // 初期データの作成
         let initialItems: [Item] = [
-            Item(descriptionText: "Initial Item 01", type: .tops, uiImageName: "jacket", true, true, true, true),
-            Item(descriptionText: "Initial Item 02", type: .bottoms, uiImageName: "pants", true, true, true, false),
-            Item(descriptionText: "Initial Item 03", type: .shoes, uiImageName: "shoes", true, true, true, true)
+            // TOPS
+            Item(descriptionText: "Initial Tops 00", type: .tops, uiImageName: "tops", true, true, true, true),
+            Item(descriptionText: "Initial Tops 01", type: .tops, uiImageName: "tops01", true, true, true, true),
+            Item(descriptionText: "Initial Tops 02", type: .tops, uiImageName: "tops02", true, true, true, true),
+            Item(descriptionText: "Initial Tops 03", type: .tops, uiImageName: "tops03", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 04", type: .tops, uiImageName: "tops04", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 05", type: .tops, uiImageName: "tops05", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 06", type: .tops, uiImageName: "tops06", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 07", type: .tops, uiImageName: "tops07", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 08", type: .tops, uiImageName: "tops08", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 09", type: .tops, uiImageName: "tops09", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 10", type: .tops, uiImageName: "tops10", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 11", type: .tops, uiImageName: "tops11", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 12", type: .tops, uiImageName: "tops12", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 13", type: .tops, uiImageName: "tops13", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 14", type: .tops, uiImageName: "tops14", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 15", type: .tops, uiImageName: "tops15", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 16", type: .tops, uiImageName: "tops16", true, true, true, true),
+//            Item(descriptionText: "Initial Tops 17", type: .tops, uiImageName: "tops17", true, true, true, true),
+            // BOTTOMS
+            Item(descriptionText: "Initial Bottoms 01", type: .bottoms, uiImageName: "bottoms01", true, true, true, true),
+            Item(descriptionText: "Initial Bottoms 02", type: .bottoms, uiImageName: "bottoms02", true, true, true, true),
+            Item(descriptionText: "Initial Bottoms 03", type: .bottoms, uiImageName: "bottoms03", true, true, true, true),
+//            Item(descriptionText: "Initial Bottoms 04", type: .bottoms, uiImageName: "bottoms04", true, true, true, true),
+//            Item(descriptionText: "Initial Bottoms 05", type: .bottoms, uiImageName: "bottoms05", true, true, true, true),
+//            Item(descriptionText: "Initial Bottoms 06", type: .bottoms, uiImageName: "bottoms06", true, true, true, true),
+//            Item(descriptionText: "Initial Bottoms 07", type: .bottoms, uiImageName: "bottoms07", true, true, true, true),
+            // SHOES
+            Item(descriptionText: "Initial Shoes 01", type: .shoes, uiImageName: "shoes01", true, true, true, true),
+            Item(descriptionText: "Initial Shoes 02", type: .shoes, uiImageName: "shoes02", true, true, true, true),
+            Item(descriptionText: "Initial Shoes 03", type: .shoes, uiImageName: "shoes03", true, true, true, true),
+//            Item(descriptionText: "Initial Shoes 04", type: .shoes, uiImageName: "shoes04", true, true, true, true),
+//            Item(descriptionText: "Initial Shoes 05", type: .shoes, uiImageName: "shoes05", true, true, true, true),
+//            Item(descriptionText: "Initial Shoes 06", type: .shoes, uiImageName: "shoes06", true, true, true, true),
+//            Item(descriptionText: "Initial Shoes 07", type: .shoes, uiImageName: "shoes07", true, true, true, true),
+//            Item(descriptionText: "Initial Shoes 08", type: .shoes, uiImageName: "shoes08", true, true, true, true),
+//            Item(descriptionText: "Initial Shoes 09", type: .shoes, uiImageName: "shoes09", true, true, true, true),
         ]
         
         // データを SwiftData に追加
