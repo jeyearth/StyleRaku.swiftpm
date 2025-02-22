00bb444ae5dc2f72cc9400b5549827fe1e168dcc
JEY HIRANO <mrjr0722@me.com>
2025-02-23 00:09:00 +0900

    初期データの整理

--- MyApp.swift
+++ MyApp.swift
@@ -43,35 +43,27 @@ struct MyApp: App {
         
         let initialItems: [Item] = [
             // TOPS
-            Item(descriptionText: "Initial Tops 01", type: .tops, uiImageName: "tops01", true, true, true, true),
-            Item(descriptionText: "Initial Tops 02", type: .tops, uiImageName: "tops02", true, true, true, true),
-            Item(descriptionText: "Initial Tops 03", type: .tops, uiImageName: "tops03", true, true, true, true),
-            Item(descriptionText: "Initial Tops 04", type: .tops, uiImageName: "tops04", true, true, true, true),
-            Item(descriptionText: "Initial Tops 06", type: .tops, uiImageName: "tops06", true, true, true, true),
-            Item(descriptionText: "Initial Tops 07", type: .tops, uiImageName: "tops07", true, true, true, true),
-            Item(descriptionText: "Initial Tops 10", type: .tops, uiImageName: "tops10", true, true, true, true),
-            Item(descriptionText: "Initial Tops 11", type: .tops, uiImageName: "tops11", true, true, true, true),
-            Item(descriptionText: "Initial Tops 13", type: .tops, uiImageName: "tops13", true, true, true, true),
-            Item(descriptionText: "Initial Tops 14", type: .tops, uiImageName: "tops14", true, true, true, true),
-            Item(descriptionText: "Initial Tops 15", type: .tops, uiImageName: "tops15", true, true, true, true),
-            Item(descriptionText: "Initial Tops 16", type: .tops, uiImageName: "tops16", true, true, true, true),
-            Item(descriptionText: "Initial Tops 17", type: .tops, uiImageName: "tops17", true, true, true, true),
+            Item(descriptionText: "Initial Tops", type: .tops, uiImageName: "tops01", true, true, true, true),
+            Item(descriptionText: "Initial Tops", type: .tops, uiImageName: "tops04", true, true, true, true),
+            Item(descriptionText: "Initial Tops", type: .tops, uiImageName: "tops06", true, true, true, true),
+            Item(descriptionText: "Initial Tops", type: .tops, uiImageName: "tops10", true, true, true, true),
+            Item(descriptionText: "Initial Tops", type: .tops, uiImageName: "tops15", true, true, true, true),
+            Item(descriptionText: "Initial Tops", type: .tops, uiImageName: "tops17", true, true, true, true),
             
             // BOTTOMS
-            Item(descriptionText: "Initial Bottoms 01", type: .bottoms, uiImageName: "bottoms01", true, true, true, true),
-            Item(descriptionText: "Initial Bottoms 02", type: .bottoms, uiImageName: "bottoms02", true, true, true, true),
-            Item(descriptionText: "Initial Bottoms 03", type: .bottoms, uiImageName: "bottoms03", true, true, true, true),
-            Item(descriptionText: "Initial Bottoms 04", type: .bottoms, uiImageName: "bottoms04", true, true, true, true),
-            Item(descriptionText: "Initial Bottoms 05", type: .bottoms, uiImageName: "bottoms05", true, true, true, true),
-            Item(descriptionText: "Initial Bottoms 06", type: .bottoms, uiImageName: "bottoms06", true, true, true, true),
+            Item(descriptionText: "Initial Bottoms", type: .bottoms, uiImageName: "bottoms01", true, true, true, true),
+            Item(descriptionText: "Initial Bottoms", type: .bottoms, uiImageName: "bottoms03", true, true, true, true),
+            Item(descriptionText: "Initial Bottoms", type: .bottoms, uiImageName: "bottoms04", true, true, true, true),
+            Item(descriptionText: "Initial Bottoms", type: .bottoms, uiImageName: "bottoms05", true, true, true, true),
+            Item(descriptionText: "Initial Bottoms", type: .bottoms, uiImageName: "bottoms06", true, true, true, true),
             
             // SHOES
-            Item(descriptionText: "Initial Shoes 02", type: .shoes, uiImageName: "shoes02", true, true, true, true),
-            Item(descriptionText: "Initial Shoes 03", type: .shoes, uiImageName: "shoes03", true, true, true, true),
-            Item(descriptionText: "Initial Shoes 05", type: .shoes, uiImageName: "shoes05", true, true, true, true),
-            Item(descriptionText: "Initial Shoes 06", type: .shoes, uiImageName: "shoes06", true, true, true, true),
-            Item(descriptionText: "Initial Shoes 07", type: .shoes, uiImageName: "shoes07", true, true, true, true),
-            Item(descriptionText: "Initial Shoes 09", type: .shoes, uiImageName: "shoes09", true, true, true, true),
+            Item(descriptionText: "Initial Shoes", type: .shoes, uiImageName: "shoes02", true, true, true, true),
+            Item(descriptionText: "Initial Shoes", type: .shoes, uiImageName: "shoes05", true, true, true, true),
+            Item(descriptionText: "Initial Shoes", type: .shoes, uiImageName: "shoes07", true, true, true, true),
+            Item(descriptionText: "Initial Shoes", type: .shoes, uiImageName: "shoes10", true, true, true, true),
+            Item(descriptionText: "Initial Shoes", type: .shoes, uiImageName: "shoes11", true, true, true, true),
+            
         ]
         
         for item in initialItems {
